549aa25f77c95bb9e18a01824cab2bd6a9d4d543
ijin <2zun@naver.com>
2021-12-08 19:22:51 +0900

    서비스 구현

---
+++ src/main/java/com/example/springMVC/service/MemberService.java
@@ -0,0 +1,43 @@
+package com.example.springMVC.service;
+
+import com.example.springMVC.Repository.MemberRepository;
+import com.example.springMVC.Repository.MemberRepositoryImpl;
+import com.example.springMVC.domain.Member;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+//@Service
+@Transactional
+public class MemberService {
+    private final MemberRepository memberRepository;
+
+    // DI
+//    @Autowired
+    public MemberService(MemberRepository memberRepository){
+        this.memberRepository = memberRepository;
+    }
+
+    public Long join(Member member){
+        validateDuplicateMember(member);
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member){
+        memberRepository.findByName(member.getName())
+                .ifPresent(m->{
+                    throw  new IllegalStateException("이미 존재하는 회원입니다.");
+                });
+    }
+
+    public List<Member> findMembers(){
+        return memberRepository.findAll();
+    }
+
+    public Optional<Member> findOne(Long memberId){
+        return memberRepository.findById(memberId);
+    }
+}
